f9d60b53da6db0fb49ffa8311dba8c68c1367ef0
Mike McKay <mfmckay@wustl.edu>
2016-05-11 17:41:10 -0500

    Fixed project access requests. Fixed bug where unverified users were unable to log in even though require verification was set to false.

--- src/main/java/org/nrg/xnat/restlet/resources/PARList.java
+++ src/main/java/org/nrg/xnat/restlet/resources/PARList.java
@@ -10,15 +10,15 @@
  */
 package org.nrg.xnat.restlet.resources;
 
-import org.nrg.xdat.security.helpers.Roles;
 import org.nrg.xft.XFTTable;
 import org.restlet.Context;
-import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Hashtable;
 
@@ -30,31 +30,27 @@ public class PARList extends SecureResource {
 	public PARList(Context context, Request request, Response response) {
 		super(context, request, response);
 		getVariants().addAll(STANDARD_VARIANTS);
-        if (!Roles.isSiteAdmin(user)) {
-            response.setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Only administrators can access the list of project access requests.");
-        }
 	}
 
 	@Override
 	public Representation represent(Variant variant) {
-		XFTTable table = new XFTTable();
-		Hashtable<String, Object> params = new Hashtable<String, Object>();
+		Hashtable<String, Object> params = new Hashtable<>();
 		try {
-			table = XFTTable
-					.Execute(
-							"SELECT par.par_id,par.proj_id,par.level,par.create_date,u.login, u.firstname, u.lastname,p.secondary_id,p.name,p.id,SUBSTRING(p.description,0,300) as description,pi.firstname || ' ' || pi.lastname FROM xs_par_table par LEFT JOIN xnat_projectData p ON par.proj_id=p.id LEFT JOIN xnat_investigatordata pi ON p.pi_xnat_investigatordata_id=pi.xnat_investigatordata_id LEFT JOIN xdat_user u ON par.approver_id=u.xdat_user_id WHERE LOWER(par.email)='"
-									+ user.getEmail().toLowerCase()
-									+ "' AND approval_date IS NULL", user
-									.getDBName(), user.getLogin());
+			final XFTTable table = XFTTable.Execute(String.format(PAR_QUERY, user.getEmail().toLowerCase()), user.getDBName(), user.getLogin());
+
+			if (table != null) {
+				params.put("totalRecords", table.size());
+			}
+
+			return representTable(table, overrideVariant(variant), params);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "An error occurred attempting to access the project invitations.");
+			_log.error("An error occurred attempting to access the project invitations for user " + user.getLogin(), e);
 		}
 
-		MediaType mt = overrideVariant(variant);
-
-		if (table != null)
-			params.put("totalRecords", table.size());
-		return representTable(table, mt, params);
+		return null;
 	}
+	private static final Logger _log = LoggerFactory.getLogger(PARList.class);
+	private static final String PAR_QUERY = "SELECT par.par_id,par.proj_id,par.level,par.create_date,u.login, u.firstname, u.lastname,p.secondary_id,p.name,p.id,SUBSTRING(p.description,0,300) as description,pi.firstname || ' ' || pi.lastname FROM xs_par_table par LEFT JOIN xnat_projectData p ON par.proj_id=p.id LEFT JOIN xnat_investigatordata pi ON p.pi_xnat_investigatordata_id=pi.xnat_investigatordata_id LEFT JOIN xdat_user u ON par.approver_id=u.xdat_user_id WHERE LOWER(par.email)='%s' AND approval_date IS NULL";
 }
