2ebb48397b00265e50eae6ea9a08d0f66d265d5b
Rick Herrick <jrherrick@wustl.edu>
2016-05-25 12:02:00 -0500

    Rearchitecting preference event handler framework.

--- src/main/java/org/nrg/xnat/event/listeners/SiteConfigPreferenceHandler.java
+++ src/main/java/org/nrg/xnat/event/listeners/SiteConfigPreferenceHandler.java
@@ -4,6 +4,8 @@ import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nrg.framework.event.entities.EventSpecificFields;
+import org.nrg.prefs.events.AbstractPreferenceHandler;
+import org.nrg.prefs.events.PreferenceHandlerMethod;
 import org.nrg.xdat.XDAT;
 import org.nrg.xdat.preferences.SiteConfigPreferenceEvent;
 import org.nrg.xdat.preferences.SiteConfigPreferences;
@@ -21,107 +23,92 @@ import reactor.bus.EventBus;
 import reactor.fn.Consumer;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ScheduledFuture;
 
-import static reactor.bus.selector.Selectors.type;
-
 @Service
-public class SiteConfigPreferenceHandler implements Consumer<Event<SiteConfigPreferenceEvent>> {
+public class SiteConfigPreferenceHandler extends AbstractPreferenceHandler<SiteConfigPreferenceEvent> {
 
-	@Inject public SiteConfigPreferenceHandler( EventBus eventBus ){
-		eventBus.on(type(SiteConfigPreferenceEvent.class), this);
+	@Inject
+	public SiteConfigPreferenceHandler(final EventBus eventBus){
+		super(SiteConfigPreferences.SITE_CONFIG_TOOL_ID, eventBus);
 	}
 
-	@Override
-	public void accept(Event<SiteConfigPreferenceEvent> event) {
-		final SiteConfigPreferenceEvent scpEvent = event.getData();
-		handleEvent(scpEvent);
-	}
-
-    public void handleEvent(SiteConfigPreferenceEvent e) {
-    	Set<EventSpecificFields> fields = e.getEventSpecificFields();
-		if(fields!=null) {
-			for (EventSpecificFields field : fields) {
-				if (StringUtils.equals(field.getFieldName(), "aliasTokenTimeout")) {
-					updateAliasTokenTimeout(e);	
-				}
-				else if (StringUtils.equals(field.getFieldName(), "aliasTokenTimeoutSchedule")) {
-					updateAliasTokenTimeout(e);
-				}
-				else if (StringUtils.equals(field.getFieldName(), "inactivityBeforeLockout")) {
-					updateInactivityBeforeLockout(e);
-				}
-				else if (StringUtils.equals(field.getFieldName(), "inactivityBeforeLockoutSchedule")) {
-					updateInactivityBeforeLockout(e);
-				}
-				else if (StringUtils.equals(field.getFieldName(), "maxFailedLoginsLockoutDuration")) {
-					updateResetFailedLogins(e);
-				}
-				else if (StringUtils.equals(field.getFieldName(), "resetFailedLoginsSchedule")) {
-					updateResetFailedLogins(e);
-				}
-			}
-		}
-    }
-	
-	private void updateAliasTokenTimeout(SiteConfigPreferenceEvent e){
-		try {
-			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
-			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
-
-			for(ScheduledFuture temp: scheduledAliasTokenTimeouts){
-				temp.cancel(false);
-			}
-
-			scheduledAliasTokenTimeouts.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new ClearExpiredAliasTokens(_template),new CronTrigger(XDAT.getSiteConfigPreferences().getAliasTokenTimeoutSchedule())));
-
-		} catch (Exception e1) {
-			_log.error("", e1);
-		}	
-	}
-
-	private void updateInactivityBeforeLockout(SiteConfigPreferenceEvent e){
-		try {
-			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
-			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
-
-			for(ScheduledFuture temp: scheduledInactivityBeforeLockout){
-				temp.cancel(false);
-			}
-
-			scheduledInactivityBeforeLockout.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new DisableInactiveUsers((new Long(SiteConfigPreferences.convertPGIntervalToSeconds(XDAT.getSiteConfigPreferences().getInactivityBeforeLockout()))).intValue(),(new Long(SiteConfigPreferences.convertPGIntervalToSeconds(XDAT.getSiteConfigPreferences().getMaxFailedLoginsLockoutDuration()))).intValue()),new CronTrigger(XDAT.getSiteConfigPreferences().getInactivityBeforeLockoutSchedule())));
-
-		} catch (Exception e1) {
-			_log.error("", e1);
-		}
-	}
-
-	private void updateResetFailedLogins(SiteConfigPreferenceEvent e){
-		try {
-			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
-			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
-
-			for(ScheduledFuture temp: scheduledResetFailedLogins){
-				temp.cancel(false);
-			}
-
-			scheduledResetFailedLogins.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new ResetFailedLogins(_template,XDAT.getSiteConfigPreferences().getMaxFailedLoginsLockoutDuration()),new CronTrigger(XDAT.getSiteConfigPreferences().getResetFailedLoginsSchedule())));
-
-		} catch (Exception e1) {
-			_log.error("", e1);
-		}
-	}
+//		if (StringUtils.equals(field.getFieldName(), "aliasTokenTimeout")) {
+//			updateAliasTokenTimeout(e);
+//		}
+//		else if (StringUtils.equals(field.getFieldName(), "aliasTokenTimeoutSchedule")) {
+//			updateAliasTokenTimeout(e);
+//		}
+//		else if (StringUtils.equals(field.getFieldName(), "inactivityBeforeLockout")) {
+//			updateInactivityBeforeLockout(e);
+//		}
+//		else if (StringUtils.equals(field.getFieldName(), "inactivityBeforeLockoutSchedule")) {
+//			updateInactivityBeforeLockout(e);
+//		}
+//		else if (StringUtils.equals(field.getFieldName(), "maxFailedLoginsLockoutDuration")) {
+//			updateResetFailedLogins(e);
+//		}
+//		else if (StringUtils.equals(field.getFieldName(), "resetFailedLoginsSchedule")) {
+//			updateResetFailedLogins(e);
+//		}
+//
+//	}
+//
+//	private void updateAliasTokenTimeout(SiteConfigPreferenceEvent e){
+//		try {
+//			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
+//			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
+//
+//			for(ScheduledFuture temp: scheduledAliasTokenTimeouts){
+//				temp.cancel(false);
+//			}
+//
+//			scheduledAliasTokenTimeouts.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new ClearExpiredAliasTokens(_template),new CronTrigger(XDAT.getSiteConfigPreferences().getAliasTokenTimeoutSchedule())));
+//
+//		} catch (Exception e1) {
+//			_log.error("", e1);
+//		}
+//	}
+//
+//	private void updateInactivityBeforeLockout(SiteConfigPreferenceEvent e){
+//		try {
+//			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
+//			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
+//
+//			for(ScheduledFuture temp: scheduledInactivityBeforeLockout){
+//				temp.cancel(false);
+//			}
+//
+//			scheduledInactivityBeforeLockout.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new DisableInactiveUsers((new Long(SiteConfigPreferences.convertPGIntervalToSeconds(XDAT.getSiteConfigPreferences().getInactivityBeforeLockout()))).intValue(),(new Long(SiteConfigPreferences.convertPGIntervalToSeconds(XDAT.getSiteConfigPreferences().getMaxFailedLoginsLockoutDuration()))).intValue()),new CronTrigger(XDAT.getSiteConfigPreferences().getInactivityBeforeLockoutSchedule())));
+//
+//		} catch (Exception e1) {
+//			_log.error("", e1);
+//		}
+//	}
+//
+//	private void updateResetFailedLogins(SiteConfigPreferenceEvent e){
+//		try {
+//			XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().setRemoveOnCancelPolicy(true);
+//			Iterator<Runnable> iter = XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").getScheduledThreadPoolExecutor().getQueue().iterator();
+//
+//			for(ScheduledFuture temp: scheduledResetFailedLogins){
+//				temp.cancel(false);
+//			}
+//
+//			scheduledResetFailedLogins.add(XDAT.getContextService().getBeansOfType(ThreadPoolTaskScheduler.class).get("taskScheduler").schedule(new ResetFailedLogins(_template,XDAT.getSiteConfigPreferences().getMaxFailedLoginsLockoutDuration()),new CronTrigger(XDAT.getSiteConfigPreferences().getResetFailedLoginsSchedule())));
+//
+//		} catch (Exception e1) {
+//			_log.error("", e1);
+//		}
+//	}
 
 	@Autowired
 	@Lazy
 	private JdbcTemplate _template;
 
 	private static final Log _log = LogFactory.getLog(SiteConfigPreferenceHandler.class);
-
-	private ArrayList<ScheduledFuture> scheduledAliasTokenTimeouts = new ArrayList<ScheduledFuture>();
 	private ArrayList<ScheduledFuture> scheduledInactivityBeforeLockout = new ArrayList<ScheduledFuture>();
+
 	private ArrayList<ScheduledFuture> scheduledResetFailedLogins = new ArrayList<ScheduledFuture>();
 }
