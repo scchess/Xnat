5a64b488e3f33281b462bb97b85c98601db87a8b
Rick Herrick <jrherrick@wustl.edu>
2016-08-15 12:46:56 -0500

    XNAT-4377 XNAT-4382 Cleaned up some dependency issues that affected build of XNAT data builder plugin. Added XnatPluginApi REST API. Added SortedProperties utility class to make it a bit easier to read properties outputs.

--- src/main/java/org/nrg/xapi/rest/settings/XnatPluginApi.java
+++ src/main/java/org/nrg/xapi/rest/settings/XnatPluginApi.java
@@ -21,17 +21,17 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
-@Api(description = "XNAT Plugin Admin API")
+@Api(description = "XNAT Plugin API")
 @XapiRestController
 @RequestMapping(value = "/plugins")
-public class XnatPluginAdminApi extends AbstractXapiRestController {
+public class XnatPluginApi extends AbstractXapiRestController {
     @Autowired
-    public XnatPluginAdminApi(final UserManagementServiceI userManagementService, final RoleHolder roleHolder, final XnatAppInfo appInfo) {
+    public XnatPluginApi(final UserManagementServiceI userManagementService, final RoleHolder roleHolder, final XnatAppInfo appInfo) {
         super(userManagementService, roleHolder);
         _appInfo = appInfo;
     }
 
-    @ApiOperation(value = "Returns a list of all of the installed XNAT plugins.", notes = "The maps returned from this call include all of the properties specified in the plugin's property file.", response = String.class, responseContainer = "Map")
+    @ApiOperation(value = "Returns a list of all of the installed and active XNAT plugins with their properties.", notes = "The maps returned from this call include all of the properties specified in the plugin's property file.", response = String.class, responseContainer = "Map")
     @ApiResponses({@ApiResponse(code = 200, message = "XNAT plugin properties successfully retrieved."),
                    @ApiResponse(code = 401, message = "Must be authenticated to access the XNAT REST API."),
                    @ApiResponse(code = 500, message = "Unexpected error")})
@@ -40,7 +40,7 @@ public class XnatPluginAdminApi extends AbstractXapiRestController {
         return new ResponseEntity<>(_appInfo.getPluginProperties(), HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Returns the requested XNAT plugin properties.", notes = "The maps returned from this call include all of the properties specified in the plugin's property file.", response = Properties.class)
+    @ApiOperation(value = "Returns the indicated XNAT plugin with its properties.", notes = "The map returned from this call include all of the properties specified in the plugin's property file.", response = Properties.class)
     @ApiResponses({@ApiResponse(code = 200, message = "XNAT plugin properties successfully retrieved."),
                    @ApiResponse(code = 401, message = "Must be authenticated to access the XNAT REST API."),
                    @ApiResponse(code = 404, message = "The requested resource wasn't found."),
