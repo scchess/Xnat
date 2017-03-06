6b9b7106a210277026808c3a9e1c4fe218e5feb9
Mike McKay <mfmckay@wustl.edu>
2017-03-06 15:48:31 -0600

    Added a REST call that Mark wanted to return a single user obtained in the same way as the profiles REST call.

--- src/main/java/org/nrg/xapi/rest/users/UsersApi.java
+++ src/main/java/org/nrg/xapi/rest/users/UsersApi.java
@@ -124,6 +124,50 @@ public class UsersApi extends AbstractXapiRestController {
         return new ResponseEntity<>(usersList, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get user profile.", notes = "The user profile function returns a user of the XNAT system with brief information.", response = User.class, responseContainer = "List")
+    @ApiResponses({@ApiResponse(code = 200, message = "A user profile."),
+            @ApiResponse(code = 401, message = "Must be authenticated to access the XNAT REST API."),
+            @ApiResponse(code = 403, message = "You do not have sufficient permissions to access the user profile."),
+            @ApiResponse(code = 500, message = "An unexpected error occurred.")})
+    @RequestMapping(value = "profile/{username}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<User> usersProfileGet(@ApiParam(value = "ID of the user to fetch", required = true) @PathVariable("username") final String username) {
+        if (_preferences.getRestrictUserListAccessToAdmins()) {
+            final HttpStatus status = isPermitted();
+            if (status != null) {
+                return new ResponseEntity<>(status);
+            }
+        }
+
+        List<User> usersList = null;
+        String regex = "^[a-zA-Z0-9_-]*$";
+        if(username.matches(regex)){
+            usersList = _jdbcTemplate.query("SELECT enabled, login AS username, xdat_user_id AS id, firstname AS firstName, lastname AS lastName, email, verified, last_modified, auth.max_login AS lastSuccessfulLogin FROM xdat_user JOIN xdat_user_meta_data ON xdat_user.xdat_user_id=xdat_user_meta_data.meta_data_id JOIN (SELECT xdat_username, max(last_successful_login) max_login FROM xhbm_xdat_user_auth GROUP BY xdat_username) auth ON xdat_user.login=auth.xdat_username WHERE xdat_user.login='"+username+"'", new RowMapper<User>() {
+                @Override
+                public User mapRow(final ResultSet resultSet, final int i) throws SQLException {
+                    Timestamp lastSuccessfulLogin = resultSet.getTimestamp("lastSuccessfulLogin");
+                    Date lastSuccessfulLoginDate = null;
+                    if(lastSuccessfulLogin!=null){
+                        lastSuccessfulLoginDate = new Date(lastSuccessfulLogin.getTime());
+                    }
+                    Timestamp lastModified = resultSet.getTimestamp("last_modified");
+                    Date lastModifiedDate = null;
+                    if(lastModified!=null){
+                        lastModifiedDate = new Date(lastModified.getTime());
+                    }
+
+                    return new User(resultSet.getInt("id"), resultSet.getString("username"), resultSet.getString("firstName"), resultSet.getString("lastName"), resultSet.getString("email"), null, null, null, true, lastModifiedDate, null, resultSet.getInt("enabled") == 1, resultSet.getInt("verified") == 1, lastSuccessfulLoginDate);
+                }
+            });
+        }
+        if(usersList!=null && usersList.size()>0) {
+            return new ResponseEntity<>(usersList.get(0), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
     @ApiOperation(value = "Get list of users who are enabled or who have interacted with the site somewhat recently.", notes = "The users' profiles function returns a list of all users of the XNAT system with brief information about each.", response = User.class, responseContainer = "List")
     @ApiResponses({@ApiResponse(code = 200, message = "A list of user profiles."),
             @ApiResponse(code = 401, message = "Must be authenticated to access the XNAT REST API."),
