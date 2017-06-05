af85f5e8b8aadf9267c0daadb4b2ce98714a964a
Mike McKay <mfmckay@wustl.edu>
2017-06-05 16:39:02 -0500

    XNAT-4954 Made searched-for subjects appear in the context of a project the user has access to.

--- src/main/java/org/nrg/xnat/turbine/modules/screens/XDATScreen_report_xnat_subjectData.java
+++ src/main/java/org/nrg/xnat/turbine/modules/screens/XDATScreen_report_xnat_subjectData.java
@@ -12,8 +12,13 @@ package org.nrg.xnat.turbine.modules.screens;
 import org.apache.log4j.Logger;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.model.XnatProjectparticipantI;
 import org.nrg.xdat.om.XnatSubjectdata;
+import org.nrg.xdat.security.helpers.Permissions;
 import org.nrg.xdat.turbine.modules.screens.SecureReport;
+
+import java.util.List;
 
 /**
  * @author Tim
@@ -28,6 +33,22 @@ public class XDATScreen_report_xnat_subjectData extends SecureReport {
     public void finalProcessing(RunData data, Context context) {
         try {
             XnatSubjectdata sub = new XnatSubjectdata(item);
+            if(context.get("project")==null) {
+                String proj = sub.getProject();
+                if (!Permissions.canReadProject(XDAT.getUserDetails(), proj)) {
+                    // If user cannot read that project, look through the projects that session is shared into. If user
+                    // can view the data in one of those projects they should view this subject from that project's context.
+                    List<XnatProjectparticipantI> list = sub.getSharing_share();
+                    for (XnatProjectparticipantI subShare : list) {
+                        if (Permissions.canReadProject(XDAT.getUserDetails(), subShare.getProject())) {
+                            proj = subShare.getProject();
+                            break;
+                        }
+                    }
+                }
+                context.put("project", proj);
+            }
+
             context.put("subject",sub);
         } catch (Exception e) {
             logger.error("",e);
