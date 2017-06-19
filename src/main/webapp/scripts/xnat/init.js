c647530483eba0a279726cdc310ca862d822225e
Mark M. Florida <markflorida@wustl.edu>
2017-06-19 17:52:48 -0500

    XNAT-4980: Powered By XNAT footer should now be pinned to the bottom of pages with little content and flow underneath pages with enough content for the page to scroll.

--- src/main/webapp/scripts/xnat/init.js
+++ src/main/webapp/scripts/xnat/init.js
@@ -32,18 +32,80 @@ var XNAT = getObject(XNAT);
     // reset or remove cookies based on query string
     (function(){
         var cookiesParam = getQueryStringValue('cookies');
-        var cookiePath = XNAT.url.context;
+        var cookiePath = XNAT.url.rootUrl();
         if (/[*/~]/.test(cookiesParam.split('|')[1] || '')) {
             cookiePath = '/';
         }
-        if (/^reset/i.test(cookiesParam)){
+        if (/^reset/i.test(cookiesParam)) {
             XNAT.cookie.resetAll({ path: cookiePath })
         }
-        if (/^remove/i.test(cookiesParam)){
+        if (/^remove/i.test(cookiesParam)) {
             XNAT.cookie.removeAll({ path: cookiePath });
         }
 
     })();
+
+
+    // add build info to page elements
+    $(function(){
+
+        console.log('buildInfo');
+
+        // add version to title attribute of XNAT logos
+        if (window.top.loggedIn !== undef && window.top.loggedIn === true) {
+
+            XNAT.cookie.set('SESSION_ACTIVE', 'true');
+
+            XNAT.xhr.get(serverRoot + '/xapi/siteConfig/buildInfo', function(data){
+
+                var version = XNAT.version = XNAT.data.version = data.version;
+                var version_string = version + ', build: ' + data.buildNumber;
+                var version_title = 'XNAT version ' + version_string;
+                var isNonRelease = /SNAPSHOT|BETA|RC/ig.test(version);
+                var build_string = ' ';
+
+                if (isNonRelease) {
+                    version_string += ' (' + data.commit + ')';
+                    build_string += '<br>' + data.buildDate;
+                }
+
+                $('#xnat_power').find('a')
+                                .attr('title', version_title)
+                                .after('<small>version ' + version_string + build_string + '</small>');
+
+                $('#header_logo').attr('title', version_title);
+
+                XNAT.app.version = version_string;
+
+            });
+        }
+        else {
+            XNAT.cookie.set('SESSION_ACTIVE', 'false');
+        }
+
+        var clicker = XNAT.event.click('#header_logo, #xnat_power > a');
+
+        // shift-click the header or footer XNAT logo to TOGGLE debug mode on/off
+        clicker.shiftKey(function(e){
+            e.preventDefault();
+            if (XNAT.cookie.get('debug') === 'on') {
+                XNAT.cookie.set('debug', 'off');
+                window.location.hash = 'debug=off';
+            }
+            else {
+                XNAT.cookie.set('debug', 'on');
+                window.location.hash = 'debug=on';
+            }
+            window.location.reload();
+        });
+
+        // alt-shift-click to open the Swagger page in a new window
+        clicker.altShift(function(e){
+            e.preventDefault();
+            XNAT.ui.popup(XNAT.url.rootUrl('/xapi/swagger-ui.html'));
+        });
+
+    });
 
 
     // this script has loaded
