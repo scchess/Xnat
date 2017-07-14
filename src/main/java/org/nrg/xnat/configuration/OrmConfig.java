0ba157ac4acd67f2db903b6395d716d9d8d877d3
Rick Herrick <jrherrick@wustl.edu>
2017-07-14 11:57:34 -0500

    Converted from hard-coded region factory to allow configuring different factories through properties.

--- src/main/java/org/nrg/xnat/configuration/OrmConfig.java
+++ src/main/java/org/nrg/xnat/configuration/OrmConfig.java
@@ -9,29 +9,31 @@
 
 package org.nrg.xnat.configuration;
 
-import org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory;
+import org.hibernate.SessionFactory;
 import org.hibernate.cache.spi.RegionFactory;
 import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.nrg.framework.beans.Beans;
 import org.nrg.framework.beans.XnatPluginBeanManager;
 import org.nrg.framework.exceptions.NrgServiceError;
 import org.nrg.framework.exceptions.NrgServiceException;
 import org.nrg.framework.orm.hibernate.AggregatedAnnotationSessionFactoryBean;
 import org.nrg.framework.orm.hibernate.PrefixedTableNamingStrategy;
-import org.nrg.framework.beans.Beans;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
 @Configuration
@@ -61,20 +63,24 @@ public class OrmConfig {
     }
 
     @Bean
-    public RegionFactory regionFactory(final Environment environment) throws NrgServiceException {
+    public RegionFactory regionFactory(final Properties hibernateProperties) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        final String regionFactoryClass = hibernateProperties.getProperty("hibernate.cache.region.factory_class", DEFAULT_REGION_FACTORY_CLASS);
+        final Class<? extends RegionFactory> clazz = Class.forName(regionFactoryClass).asSubclass(RegionFactory.class);
         try {
-            return new SingletonEhCacheRegionFactory(hibernateProperties(environment).getObject());
-        } catch (IOException e) {
-            throw new NrgServiceException(NrgServiceError.Unknown, "An error occurred trying to retrieve the Hibernate properties", e);
+            @SuppressWarnings("JavaReflectionMemberAccess")
+            final Constructor<? extends RegionFactory> constructor = clazz.getConstructor(Properties.class);
+            return constructor.newInstance(hibernateProperties);
+        } catch (NoSuchMethodException e) {
+            return clazz.newInstance();
         }
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory(final Environment environment, final DataSource dataSource, final XnatPluginBeanManager manager) throws NrgServiceException {
+    public FactoryBean<SessionFactory> sessionFactory(final Environment environment, final DataSource dataSource, final RegionFactory regionFactory, final XnatPluginBeanManager manager) throws NrgServiceException {
         try {
             final AggregatedAnnotationSessionFactoryBean bean = new AggregatedAnnotationSessionFactoryBean(manager, XNAT_ENTITIES_PACKAGES);
             bean.setDataSource(dataSource);
-            bean.setCacheRegionFactory(regionFactory(environment));
+            bean.setCacheRegionFactory(regionFactory);
             bean.setHibernateProperties(hibernateProperties(environment).getObject());
             bean.setNamingStrategy(namingStrategy());
             return bean;
@@ -84,8 +90,8 @@ public class OrmConfig {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(final Environment environment, final DataSource dataSource, final XnatPluginBeanManager manager) throws NrgServiceException {
-        return new HibernateTransactionManager(sessionFactory(environment, dataSource, manager).getObject());
+    public PlatformTransactionManager transactionManager(final SessionFactory sessionFactory) throws NrgServiceException {
+        return new HibernateTransactionManager(sessionFactory);
     }
 
     @Bean
@@ -94,11 +100,13 @@ public class OrmConfig {
     }
 
     private static final String     XNAT_ENTITIES_PACKAGES       = "META-INF/xnat/entities/**/*-entity-packages.txt";
+    private static final String     DEFAULT_REGION_FACTORY_CLASS = "org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory";
     private static final Properties DEFAULT_HIBERNATE_PROPERTIES = new Properties() {{
         setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
         setProperty("hibernate.hbm2ddl.auto", "update");
         setProperty("hibernate.show_sql", "false");
         setProperty("hibernate.cache.use_second_level_cache", "true");
+        setProperty("hibernate.cache.region.factory_class", DEFAULT_REGION_FACTORY_CLASS);
         setProperty("hibernate.cache.use_query_cache", "true");
     }};
 
