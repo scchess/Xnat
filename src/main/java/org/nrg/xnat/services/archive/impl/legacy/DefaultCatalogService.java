28f6dbb89b45e3e16bbbc7e4bd39753049aa43fb
Mike McKay <mfmckay@wustl.edu>
2017-09-11 17:37:50 -0500

    XXX-65

--- src/main/java/org/nrg/xnat/services/archive/impl/legacy/DefaultCatalogService.java
+++ src/main/java/org/nrg/xnat/services/archive/impl/legacy/DefaultCatalogService.java
@@ -1003,7 +1003,7 @@ public class DefaultCatalogService implements CatalogService {
                 final String sessionLabel = URLEncoder.encode(resource.get("session_label").toString(), "UTF-8");
                 final String proj = URLEncoder.encode(resource.get("project").toString(), "UTF-8");
                 try{
-                    if(Permissions.canReadProject(user,proj) && Permissions.canRead(user, resource.get("xsi").toString()+"/project", resource.get("assessor_id"))) {
+                    if(Permissions.canReadProject(user,proj) && Permissions.canRead(user, resource.get("xsi").toString()+"/project", proj)) {
                         entry.setName(getPath(options, project, subject, sessionLabel, "assessors", assessorLabel, "resources", resourceLabel));
                         entry.setUri(StrSubstitutor.replace("/archive/experiments/${session_id}/assessors/${assessor_id}/out/resources/${resource_label}/files", resource));
                         _log.debug("Created session assessor entry for project {} session {} assessor {} resource {} with name {}: {}", project, sessionId, assessorLabel, resourceLabel, entry.getName(), entry.getUri());
