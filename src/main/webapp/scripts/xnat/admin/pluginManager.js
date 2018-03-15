0d3de32559073aa99e016df7778ca7e9e5376d24
Mark M. Florida <markflorida@wustl.edu>
2018-03-15 14:03:41 -0500

    Improve display of plugin list on site admin page (update from other branch).

--- src/main/webapp/scripts/xnat/admin/pluginManager.js
+++ src/main/webapp/scripts/xnat/admin/pluginManager.js
@@ -15,10 +15,57 @@ $(function(){
 
     console.log('pluginManager.js');
 
-    var $body = $('body');
+    // spawning this table in js for more flexibility
+    XNAT.table.dataTable([], {
+        id: 'plugin-list-table',
+        load: '/xapi/plugins',
+        columns: {
+            id: '~data-id',
+            beanName: '~data-bean',
+            pluginInfo: {
+                label: 'Plugin Info',
+                filter: true,
+                apply: function(){
+                    var item      = this;
+                    var itemTable = XNAT.table({
+                        className: 'xnat-table clean compact borderless'
+                    });
+                    itemTable.tbody();
+                    function itemRow(TH, TD){
+                        var _TH = spawn('b', TH);
+                        itemTable.tr().td({ className: 'text-right nowrap valign-top' }, _TH).td({}, TD);
+                    }
+                    itemRow('Plugin Name:', spawn('b', item.name));
+                    itemRow('Plugin ID:', item.id);
+                    itemRow('Plugin Version:', item.version);
+                    itemRow('Plugin Class:', item.pluginClass);
+                    itemRow('Description:', item.description);
+                    return itemTable.table;
+                }
+            },
+            viewPluginInfo: {
+                label: 'Plugin JSON',
+                value: "&ndash;", // this gets overwritten with 'html' below
+                className: 'center',
+                // function that accepts current item as sole argument and parent object as 'this'
+                // returned value will be new value - if nothing's returned, value is passed through
+                // apply: "{(  function(){ return formatJSON(this) }  )}"
+                html: '<a href="#!" class="view-plugin-info link nowrap">View JSON</a>'
+            }
+        },
+        messages: {
+            noData: '' +
+            'There are no plugins installed in this XNAT. Try searching for plugins on ' +
+            '<a href="http://marketplace.xnat.org/plugins/" title="XNAT Marketplace" target="_blank">XNAT Marketplace</a>.',
+            error: 'An error occurred retrieving information for installed plugins.'
+        }
+    }).render('div#plugin-table-container');
+
 
     // open dialog to view JSON
-    $body.on('click', 'a.view-plugin-info', function(){
+    $(document).on('click', 'a.view-plugin-info', function(e){
+
+        e.preventDefault();
 
         var _id  = $(this).closest('tr').dataAttr('id');
         var _url = XNAT.url.restUrl('/xapi/plugins/' + _id);
